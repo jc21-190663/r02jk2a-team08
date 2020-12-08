01dadb1479703532288bfb0e72eac60040c18da5
190663 <190663@e0505-31.JC-21.local>
2020-12-08 14:46:08 +0900

    出席番号を登録

--- src/Kadai02.java
+++ src/Kadai02.java
@@ -1,14 +1,31 @@
 
 public class Kadai02 {
 
-	/**
-	 * 課題02
-	 * 文字列の中に、指定されたアルファベットが何文字入っているかを返すメソッド
-	 * @param s 文字列
-	 * @param c 文字
-	 * @return sの中に入っていたcの文字数
-	 */
 	int countString(String s,char c) {
-		return -1;
+
+		char[] array = s.toCharArray();
+
+		int count = 0;
+
+		for(int i= 0; i < array.length; i++) {
+			//arrayの長さ分ループ
+
+			if(c ==array[i]) {
+				//c(指定したアルファベット)と一致したら↓の処理
+
+				count++;
+				//一致したら+1する。
+			}
+		}
+		return count;
+		//
 	}
 }
+
+/**
+ * 課題02
+ * 文字列の中に、指定されたアルファベットが何文字入っているかを返すメソッド
+ * @param s 文字列
+ * @param c 文字
+ * @return sの中に入っていたcの文字数
+ */
